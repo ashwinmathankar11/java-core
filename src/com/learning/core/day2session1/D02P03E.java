e5ee2bd81d2bf8471c63fd9109a4f7b8b3a92550
ashwinmathankar11 <ashwinmathankar11@gmail.com>
2024-04-04 12:41:21 +0530

    this is second commit

---
+++ src/com/learning/core/day2session1/D02P03E.java
@@ -0,0 +1,21 @@
+package com.learning.core.day2session1;
+
+import java.util.*;
+
+public class D02P03E {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter any value to find the factorial value: ");
+		
+		int num = sc.nextInt();
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i) {
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+        
+	}
+
+}
